ea441e0006b1a7974d515fa843cdb3eb26e1aae7
Ahmet Enes GÜRBÜZ <32681118+enesgurbuz@users.noreply.github.com>
2021-05-10 15:30:43 +0300

    Update proje.c

--- proje.c
+++ proje.c
@@ -4,6 +4,7 @@
 #include "jrb.h"
 #include "fields.h"
 #include "jval.h"
+#define MAX_SIZE 500
 char* foo(int baslangic,int bitis,char *s){
 	char *ptr = malloc(sizeof(char)*(bitis-baslangic));
 	int i,j =0;
@@ -17,25 +18,25 @@ char* foo(int baslangic,int bitis,char *s){
 	}
 	return ptr;
 }
-void fill_jrb(){
+JRB fill_jrb(){
     Jval val;
     IS is;
     JRB b,bn;
     char *arr,*arr1;
     int i,satir=0,j=0;
     b = make_jrb();
-    is = new_inputstruct("ornek2/.kilit");
+    is = new_inputstruct("11/.kilit");
     if (is == NULL) {
-        perror("ornek2/.kilit");
+        perror("11/.kilit");
         exit(1);
     }
     
     while(get_line(is) >= 0){
     	satir++;
     }
-     is = new_inputstruct("ornek2/.kilit");
+     is = new_inputstruct("11/.kilit");
     if (is == NULL) {
-        perror("ornek2/.kilit");
+        perror("11/.kilit");
         exit(1);
     }
 
@@ -62,25 +63,87 @@ void fill_jrb(){
                 	arr1 = malloc(sizeof(char)*strlen(val.s));
                 	strcpy(arr1,val.s);
                 	(void) jrb_insert_str(b, strdup(arr), new_jval_s(arr1));
-                	
                 }
-               
      		}
      	}
      	j++;
      }
-	jrb_traverse(bn, b) {
-	    printf("%s %s\n", jval_s(bn->key),jval_s(bn->val));
-	  }
+	
     jettison_inputstruct(is);
+    return b;
 }
 
 void descripted(const char *giris_metin , const char *cikis_metin){
-    fill_jrb();
-    printf("ddd\n");
+    IS is;
+	int i,j,size=0;
+	FILE *fp = fopen(cikis_metin,"w+");
+	char input_metin[MAX_SIZE][20];
+	JRB bn;
+    JRB b = fill_jrb();
+	is = new_inputstruct(giris_metin);
+	if (is == NULL) {
+		perror(giris_metin);
+		exit(1);
+	}
+
+	while(get_line(is) >= 0) {
+		for (i = 0; i < is->NF; i++) {
+	  		if (size >= MAX_SIZE)
+			{
+				printf("500 kelimeden fazla input ..!\n");
+				return;
+			}
+			else{
+		  		strcpy(input_metin[size],is->fields[i]);
+		  		size++;
+			}
+		}
+	}
+	for (i = 0; i < size; ++i)
+	{
+		bn = jrb_find_str(b, input_metin[i]) ;
+		if (bn != NULL )
+		{
+			fprintf(fp,"%s ", jval_s(bn->val));
+		}
+	}
 }
+
 void encripted(const char *giris_metin , const char *cikis_metin){
-	printf("eee\n");
+	IS is;
+	int i,j,size=0;
+	FILE *fp = fopen(cikis_metin,"w+");
+	char input_metin[MAX_SIZE][20];
+	JRB bn,ptr;
+    JRB b = fill_jrb();
+  
+	is = new_inputstruct(giris_metin);
+	if (is == NULL) {
+	perror(giris_metin);
+	exit(1);
+	}
+	while(get_line(is) >= 0) {
+		for (i = 0; i < is->NF; i++) {
+	  		if (size >= MAX_SIZE)
+			{
+				printf("500 kelimeden fazla input ..!\n");
+				return;
+			}
+			else{
+		  		strcpy(input_metin[size],is->fields[i]);
+		  		size++;
+			}
+		}
+	}
+	
+	for (i = 0; i < size; ++i)
+	{
+		for(ptr = jrb_first(b); ptr != jrb_nil(b); ptr = jrb_next(ptr)){
+			if(strcmp(input_metin[i],jval_s(ptr->val)) == 0){
+				fprintf(fp,"%s ", jval_s(ptr->key));
+			}
+		}
+	}
 }
 int main(int argc, char const *argv[]){
     if (argc != 5)
